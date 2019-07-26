4b34a4e9999cc3288197e695234d7b9260fdf3f3
Scars and Bullets <sbrough@gmail.com>
2019-07-25 21:42:18 -0600

    Updates

---
+++ precache-manifest.6a995f249cb0dd7f4f06472810a271e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d80f7ec1c725121daca89afc4fd99fd1",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "f36659743ae29ca2a41a",
+    "url": "/react-portfolio/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-portfolio/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "f36659743ae29ca2a41a",
+    "url": "/react-portfolio/static/js/main.ac96d2aa.chunk.js"
+  },
+  {
+    "revision": "94872cf7e81768d148a3",
+    "url": "/react-portfolio/static/js/runtime~main.f974a8eb.js"
+  }
+]);
